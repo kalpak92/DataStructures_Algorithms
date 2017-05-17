92093d1c0fad183c19385c2df7840a1301468892
Kalpak Seal <kals9seals@gmail.com>
2017-05-18 00:05:25 +0530

    Add files via upload

---
+++ Linked List/Doubly Linked List/DeletingANodeInDLL.py
@@ -0,0 +1,92 @@
+__author__ = 'Kalpak Seal'
+import gc
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+    def get_data(self):
+        return self.data
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+
+        new_node.next = self.head
+
+        if self.head is not None:
+            self.head.prev = new_node
+
+        self.head = new_node
+
+    def insert_after_given_node(self, prev_node, data):
+        if prev_node is None:
+            print("The previous node given, cannot be NULL")
+            return
+
+        new_node = Node(data)
+
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+        new_node.prev = prev_node
+        if new_node.next is not None:
+            new_node.next.prev = new_node
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        current_node = self.head
+
+        while current_node.next is not None:
+            current_node = current_node.next
+
+        current_node.next = new_node
+        new_node.prev = current_node
+
+        return
+
+    def delete_a_node(self, del_node):
+        if self.head is None or del_node is None:
+            return
+        if self.head == del_node:
+            self.head = del_node.next
+
+        if del_node.next is not None:
+            del_node.next.prev = del_node.prev
+
+        if del_node.prev is not  None:
+            del_node.prev.next = del_node.next
+        gc.collect()
+
+    def print_DLL(self, head):
+        while head is not None:
+            print(head.get_data(), end=' ')
+            head = head.next
+        print()
+
+def main():
+    d = DoublyLinkedList()
+    d.head = Node(1)
+    d.insert_at_end(2)
+    d.insert_at_end(3)
+    d.insert_at_beginning(4)
+    d.insert_after_given_node(d.head.next,5)
+    d.print_DLL(d.head)
+
+    print ("Deleting a node")
+    d.delete_a_node(d.head.next)
+
+    d.print_DLL(d.head)
+
+if __name__=='__main__':
+    main()
---
+++ Linked List/Doubly Linked List/ReverseADLL.py
@@ -0,0 +1,89 @@
+__author__ = 'Kalpak Seal'
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+    def get_data(self):
+        return self.data
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+
+        new_node.next = self.head
+
+        if self.head is not None:
+            self.head.prev = new_node
+
+        self.head = new_node
+
+    def insert_after_given_node(self, prev_node, data):
+        if prev_node is None:
+            print("The previous node given, cannot be NULL")
+            return
+
+        new_node = Node(data)
+
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+        new_node.prev = prev_node
+        if new_node.next is not None:
+            new_node.next.prev = new_node
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        current_node = self.head
+
+        while current_node.next is not None:
+            current_node = current_node.next
+
+        current_node.next = new_node
+        new_node.prev = current_node
+
+        return
+
+    def print_DLL(self, head):
+        while head is not None:
+            print(head.get_data(), end=' ')
+            head = head.next
+        print ()
+
+def reverse_a_DLL(head):
+    temp = None
+    current_node = head
+
+    while current_node is not None:
+        temp = current_node.prev
+        current_node.prev = current_node.next
+        current_node.next = temp
+        current_node = current_node.prev
+
+    if temp is not None:
+        head = temp.prev
+        return head
+
+def main():
+    d = DoublyLinkedList()
+    d.head = Node(1)
+    d.insert_at_end(2)
+    d.insert_at_end(3)
+    d.insert_at_beginning(4)
+    d.insert_after_given_node(d.head.next,5)
+    d.print_DLL(d.head)
+
+    print("Reversing the Doubly Linked List")
+    d.print_DLL(reverse_a_DLL(d.head))
+
+if __name__=='__main__':
+    main()
