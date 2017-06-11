3bc3f8ddcc5377167fc269ae6a2390586364a992
Kalpak Seal <kals9seals@gmail.com>
2017-06-11 13:05:03 +0530

    Create ModularInverse.py

--- Algorithms/Mathematical Algorithms/ModularInverse.py
+++ Algorithms/Mathematical Algorithms/ModularInverse.py
@@ -1,5 +1,23 @@
 __author__ = 'Kalpak Seal'
 
+'''
+ax + by = gcd(a, b)
+
+To find multiplicative inverse of ‘a’ under ‘m’, we put b = m in above formula. 
+
+Since we know that a and m are relatively prime, we can put value of gcd as 1.
+ ax + my = 1 
+ 
+If we take modulo m on both sides, we get
+ ax + my ≡ 1 (mod m)
+ 
+We can remove the second term on left side as ‘my (mod m)’ would always be 0 for an integer y.
+ ax  ≡ 1 (mod m) 
+ 
+So the ‘x’ that we can find using Extended Euclid Algorithm is multiplicative inverse of ‘a’
+
+'''
+
 def extended_euclid(a, b):
     x0, x1, y0, y1 = 1, 0, 0, 1
 
