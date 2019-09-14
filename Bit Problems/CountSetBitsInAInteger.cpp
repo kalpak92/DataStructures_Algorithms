b075c436a777cf602310e4d12a023909d28c6ef1
Kalpak <kals9seals@gmail.com>
2019-09-14 01:05:41 -0400

    Swap bits of a number at position i and j

--- Bit Problems/CountSetBitsInAInteger.cpp
+++ Bit Problems/CountSetBitsInAInteger.cpp
@@ -1,4 +1,6 @@
-#include <stdio.h>
+#include <iostream>
+
+using namespace std;
  
 /* Function to get no of set bits in binary
    representation of passed binary no. */
@@ -10,7 +12,7 @@ unsigned int countSetBits(int n)
     unsigned int count = 0;
     while (n)
     {
-      n &= (n-1) ;
+      n &= (n-1) ; //clears the lowest set bit in n.
       count++;
     }
     return count;
@@ -19,9 +21,9 @@ unsigned int countSetBits(int n)
 /* Program to test function countSetBits */
 int main()
 {
-    int i = 15;
-    int y = countSetBits(i);
-    printf("%d ", i^y);
-    // printf("%d", countSetBits(i));
-    return 0;
+  int i = 15;
+  int y = countSetBits(i);
+  cout << (i^y) << endl;
+  
+  return 0;
 }
--- Bit Problems/SwapAllOddAndEvenBitsOfANumber.cpp
+++ Bit Problems/SwapAllOddAndEvenBitsOfANumber.cpp
@@ -1,13 +1,14 @@
-// C program to swap even and odd bits of a given number
-#include <stdio.h>
- 
+// CPP program to swap even and odd bits of a given number.
+#include <iostream>
+using namespace std;
+
 unsigned int swapBits(unsigned int x)
 {
     // Get all even bits of x
-    unsigned int even_bits = x & 0xAAAAAAAA; 
+    unsigned int even_bits = x & 0xAAAAAAAA;    // A stands for 10101010 in hex.
  
     // Get all odd bits of x
-    unsigned int odd_bits  = x & 0x55555555; 
+    unsigned int odd_bits  = x & 0x55555555;    // 5 stands for 01010101 in hex.
  
     even_bits >>= 1;  // Right shift even bits
     odd_bits <<= 1;   // Left shift odd bits
@@ -21,7 +22,7 @@ int main()
     unsigned int x = 23; // 00010111
  
     // Output is 43 (00101011)
-    printf("%u ", swapBits(x));
+    cout << swapBits(x) << endl;
  
     return 0;
 }
