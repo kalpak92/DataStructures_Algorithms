437d7959366b6051d7243784847400abf34df6fc
Kalpak <kals9seals@gmail.com>
2019-10-26 21:05:41 -0400

    Leetcode 692. top k frequent words

---
+++ LeetCode/692. Top K frequent words/Solution.java
@@ -0,0 +1,56 @@
+/**
+ * Given a non-empty list of words, return the k most frequent elements.
+ * 
+ * Your answer should be sorted by frequency from highest to lowest. 
+ * If two words have the same frequency, then the word with the lower alphabetical order comes first.
+ * 
+ * Example 1:
+ * Input: ["i", "love", "leetcode", "i", "love", "coding"], k = 2
+ * Output: ["i", "love"]
+ * 
+ * Explanation: "i" and "love" are the two most frequent words.
+ * Note that "i" comes before "love" due to a lower alphabetical order.
+ * 
+ * Example 2:
+ * Input: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"], k = 4
+ * Output: ["the", "is", "sunny", "day"]
+ * 
+ * Explanation: "the", "is", "sunny" and "day" are the four most frequent words
+ * with the number of occurrence being 4, 3, 2 and 1 respectively.
+ * 
+ * Note:
+ * You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+ * Input words contain only lowercase letters.
+ * Follow up:
+ *      Try to solve it in O(n log k) time and O(n) extra space.
+ */
+
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        HashMap<String, Integer> count = new HashMap<>();
+        
+        for(String w : words)
+            count.put(w, count.getOrDefault(w, 0) + 1);
+        
+        PriorityQueue<String> heap = new PriorityQueue<String>(
+                                        (w1, w2) -> count.get(w1).equals(count.get(w2)) 
+                                        ? w2.compareTo(w1) : count.get(w1) - count.get(w2));
+        
+        for(String word : count.keySet())
+        {
+            heap.offer(word);
+            
+            if(heap.size() > k)
+                heap.poll();
+        }
+        
+        List<String> result = new ArrayList<>();
+        
+        while(!heap.isEmpty())
+        {
+            result.add(0, heap.poll());
+        }
+        
+        return result;
+    }
+}
