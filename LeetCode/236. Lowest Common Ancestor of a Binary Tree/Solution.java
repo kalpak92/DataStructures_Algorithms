f7cf6348d83f89eb414af514bb685078874f1a92
Kalpak <kals9seals@gmail.com>
2019-11-16 23:47:40 -0500

    LCA of binary tree revisited

--- LeetCode/236. Lowest Common Ancestor of a Binary Tree/Solution.java
+++ LeetCode/236. Lowest Common Ancestor of a Binary Tree/Solution.java
@@ -35,19 +35,15 @@ class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null)
             return null;
-        
         if (root == p || root == q)
             return root;
         
         TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right  = lowestCommonAncestor(root.right, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
         
-        if (left == null && right == null)
-            return null;
-        
-        if (left != null && right != null)
-            return root;
-        
-        return (left != null) ? left : right;
+        // going bottom-up now. 
+        // If the left is null or the right is null, then return their alternate. -- This is the overlapping condition where x is the LCA of y or vice-versa.
+        // Or else, return root.
+        return (left == null ? right : (right == null) ? left : root);
     }
 }
