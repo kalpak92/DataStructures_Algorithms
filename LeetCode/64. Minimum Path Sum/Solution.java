22ca307910c929c6a332c85a26db261ea741bb91
Kalpak <kals9seals@gmail.com>
2019-12-03 22:40:09 -0500

    leetcode 64. minimum path sum

---
+++ LeetCode/64. Minimum Path Sum/Solution.java
@@ -0,0 +1,42 @@
+/**
+ * Given a m x n grid filled with non-negative numbers, 
+ * find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+ * 
+ * Note: You can only move either down or right at any point in time.
+ * 
+ * Example:
+ * Input:
+ * [
+ *   [1,3,1],
+ *   [1,5,1],
+ *   [4,2,1]
+ * ]
+ * 
+ * Output: 7
+ * Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+ */
+
+class Solution {
+    public int minPathSum(int[][] grid) {
+        if(grid == null || grid.length == 0)
+            return 0;
+        
+        int[][] dp = new int [grid.length][grid[0].length];
+        
+        for(int i = 0; i < dp.length; i++)
+        {
+            for(int j = 0; j < dp[i].length; j++)
+            {
+                dp[i][j] += grid[i][j];
+                
+                if(i > 0 && j > 0)
+                    dp[i][j] += Math.min(dp[i - 1][j], dp[i][j - 1]);
+                else if(i > 0)
+                    dp[i][j] += dp[i - 1][j];
+                else if(j > 0)
+                    dp[i][j] += dp[i][j - 1];
+            }
+        }
+        return dp[dp.length - 1][dp[0].length - 1];
+    }
+}
--- LeetCode/README.md
+++ LeetCode/README.md
@@ -180,6 +180,7 @@
 - [x] [Word Break](https://github.com/kalpak92/DataStructures_Algorithms/tree/master/LeetCode/139.%20Word%20Break)
 - [x] [Climbing Stairs](https://github.com/kalpak92/DataStructures_Algorithms/tree/master/LeetCode/70.%20Climbing%20Stairs)
 - [x] [Decode Ways](https://github.com/kalpak92/DataStructures_Algorithms/tree/master/LeetCode/91.%20Decode%20Ways)
+- [x] [Minimum Path Sum]()
 - [x] [Unique Paths](https://github.com/kalpak92/DataStructures_Algorithms/tree/master/LeetCode/62.%20Unique%20Paths)
 - [x] [Unique Paths II](https://github.com/kalpak92/DataStructures_Algorithms/tree/master/LeetCode/63.%20Unique%20Paths%20II)
-- [x] [Coin Change]()
+- [x] [Coin Change](https://github.com/kalpak92/DataStructures_Algorithms/tree/master/LeetCode/322.%20Coin%20Change)
