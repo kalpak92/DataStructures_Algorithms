3b03f1a22ce3ba8f5e2d72ddc2d4b9a04caf7ecf
amitbhadra <amitbhadra@192.168.0.144>
2020-01-28 03:44:00 -0500

    (+) new solution

--- LeetCode/687. Longest Univalue Path/Solution.java
+++ LeetCode/687. Longest Univalue Path/Solution.java
@@ -36,6 +36,8 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+
 class Solution {
     int result = 0;
     
@@ -71,4 +73,37 @@ class Solution {
         
         return Math.max(leftMax, rightMax); // return back the maximum of the left and right subtree to the previous state.
     }
+}
+
+
+
+
+//https://leetcode.com/problems/longest-univalue-path/discuss/108175/java-solution-with-global-variable
+class Solution {
+	
+	int len = 0; // global variable
+	
+	public int longestUnivaluePath(TreeNode root) {
+		
+	    if (root == null) return 0;
+	    
+	    len = 0;
+	    getLen(root, root.val);
+	    return len;
+	}
+
+	private int getLen(TreeNode node, int val) {
+		
+	    if (node == null) return 0;
+	    
+	    int left = getLen(node.left, node.val);
+	    int right = getLen(node.right, node.val);
+	    
+	    len = Math.max(len, left + right);
+	    
+	    if (val == node.val)  
+	    	return Math.max(left, right) + 1;
+	    
+	    return 0;
+	}
 }
