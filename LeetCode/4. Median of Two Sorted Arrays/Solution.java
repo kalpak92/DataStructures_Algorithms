3f758c26106350465e2118c4e062769468e5088c
Kalpak <kals9seals@gmail.com>
2020-08-27 23:46:40 -0400

    median of two sorted arrays

--- LeetCode/4. Median of Two Sorted Arrays/Solution.java
+++ LeetCode/4. Median of Two Sorted Arrays/Solution.java
@@ -19,53 +19,50 @@
 
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        // we consider that length of arr1 is less than arr2. If that is violated, swap the two and start over.
         if(nums1.length > nums2.length)
             return findMedianSortedArrays(nums2, nums1);
-        
+
         int len1 = nums1.length;
         int len2 = nums2.length;
-        
-        int low  = 0;
-        int high = len1;
-        
-        while(low <= high)
-        {
-            int partitionX = low + (high - low) /2;
-            int partitionY = (len1 + len2 + 1) / 2 - partitionX;
-            
-            //if partitionX is 0 it means nothing is there on left side. Use -INF for maxLeftX
-            //if partitionX is length of input then there is nothing on right side. Use +INF for minRightX
+
+        int left = 0;
+        int right = len1;
+
+        while(left <= right) {
+            // Compute the mid from nums1
+            int partitionX = left + (right - left)/2;
+            // The partition of nums2 will be (len1 + len2 + 1) / 2 - partitionX; 
+            // because partitionX + partitionY is (len1 + len2 + 1)/2
+            int partitionY = (len1 + len2 + 1)/2 - partitionX;
+
             int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
             int minRightX = (partitionX == len1) ? Integer.MAX_VALUE : nums1[partitionX];
 
             int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
             int minRightY = (partitionY == len2) ? Integer.MAX_VALUE : nums2[partitionY];
-            
-            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
-                // We have partitioned array at correct place
-                // Now get max of left elements and min of right elements to get the median in case of even length combined array size
-                // or get max of left for odd length combined array size.
-                if ((len1 + len2) % 2 == 0) 
-                {
-                    return ((double)Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY))/2;
-                } 
-                else 
-                {
-                    return (double)Math.max(maxLeftX, maxLeftY);
-                }
-            }
-            else if (maxLeftX > minRightY) 
-            {   // we are too far on right side for partitionX. Go on left side.
-                high = partitionX - 1;
-            } 
-            else 
-            {   // we are too far on left side for partitionX. Go on right side.
-                low = partitionX + 1;
-            }
+
+            /**
+             * Moment of Truth:
+             * Check if maxLeftX <= minRightY and maxLeftY <= minRightX.
+             * If so, compute the median and return
+             */
+
+             if(maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                 /**
+                  * Check the length of the entire two arrays and decide based on odd or even length.
+                  */
+                  if((len1 + len2)%2 == 0)  // If it is even, return the average
+                    return ((double) Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY))/2.0;
+                else
+                    return ((double) Math.max(maxLeftX, maxLeftY));
+             }
+             else if (maxLeftX > minRightY) // Need to go left
+                right = partitionX - 1;
+            else
+                left = partitionX + 1;
+
         }
-        
-        // Only we we can come here is if input arrays were not sorted. Throw in that scenario.
         throw new IllegalArgumentException();
+        
     }
 }
