3ef771c75aa4d31f259f32ebde8406fecf1951da
Kalpak <kals9seals@gmail.com>
2020-08-29 17:42:28 -0400

    meeting rooms 2

--- LeetCode/253. Meeting Rooms II/Solution.java
+++ LeetCode/253. Meeting Rooms II/Solution.java
@@ -16,22 +16,28 @@ class Solution {
         if(intervals.length < 1)
             return 0;
         
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        // Sort the array based on the starting time of the meeting
+        Arrays.sort(intervals, (a, b) -> (a[0] -  b[0]));
         
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        minHeap.add(intervals[0]);
+        // Create a minHeap to get the meeting that finishes first
+        // Check if the intervals overlap, then we need a separate room, else merge them
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> (a[1] - b[1]));
+        minHeap.offer(intervals[0]);
         
-        for(int i = 1; i < intervals.length; i++)
-        {
+        for(int i = 1; i < intervals.length; i++) {
+            int[] previous = minHeap.poll();
             int[] current = intervals[i];
-            int[] earliest = minHeap.remove();
             
-            if(current[0] >= earliest[1])
-                earliest[1] = current[1];
-            else
-                minHeap.add(current);
-            
-            minHeap.add(earliest);
+            // Check if the end time of the previous meeting is <= the start time of the current meeting
+            if(previous[1] <= current[0]) {
+                // Merge the two, as we can reuse the meeting rooms
+                previous[1] = current[1];
+            }
+            else {
+                // we need a separate room.
+                minHeap.offer(current);
+            }
+            minHeap.offer(previous);
         }
         return minHeap.size();
     }
