d62ba483c9ccf0ace9a2b6156c297008c63e475b
Kalpak <kals9seals@gmail.com>
2020-08-29 01:48:46 -0400

    merge intervals

--- LeetCode/56. Merge Intervals/Solution.java
+++ LeetCode/56. Merge Intervals/Solution.java
@@ -13,28 +13,27 @@
 
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if (intervals.length <= 1)
-		return intervals;
-
-	// Sort by ascending starting point
-	Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
-
-	List<int[]> result = new ArrayList<>();
-
-	int[] newInterval = intervals[0];
-	result.add(newInterval);
-		
-        for (int[] interval : intervals) 
-        {    
-		if (interval[0] <= newInterval[1])              // Overlapping intervals, move the end if needed
-			newInterval[1] = Math.max(newInterval[1], interval[1]);
-		else {                                         // Disjoint intervals, add the new interval to the list
-			newInterval = interval;
-			result.add(newInterval);
-			// System.out.println(newInterval[0] + " " + newInterval[1]);
-		}
-	}
-
-	return result.toArray(new int[result.size()][]);
+        if(intervals.length <= 1)
+            return intervals;
+        
+        Arrays.sort(intervals, (a, b) -> (a[0] - b[0]));
+        
+        List<int[]> result = new ArrayList<>();
+        
+        int[] runningInterval = intervals[0];
+        result.add(runningInterval);
+        
+        for(int[] interval : intervals) {
+            if(interval[0] <= runningInterval[1]) {
+                // Need to merge the intervals
+                runningInterval[1] = Math.max(interval[1], runningInterval[1]);
+            }
+            else {
+                // The intervals are disjoint.
+                runningInterval = interval;
+                result.add(runningInterval);
+            }
+        }
+        return result.toArray(new int [result.size()][]);
     }
 }
