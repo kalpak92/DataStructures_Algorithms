31ab0728d8e3cfe6c04d9e119691b340d402c32e
Kalpak <kals9seals@gmail.com>
2021-01-20 21:08:40 -0500

    leetcode 986. interval list intersections

---
+++ LeetCode/986. Interval List Intersections/Solution.java
@@ -0,0 +1,62 @@
+/**
+ *
+ * You are given two lists of closed intervals, firstList and secondList, where firstList[i] = [starti, endi] and secondList[j] = [startj, endj].
+ * Each list of intervals is pairwise disjoint and in sorted order.
+ *
+ * Return the intersection of these two interval lists.
+ *
+ * A closed interval [a, b] (with a < b) denotes the set of real numbers x with a <= x <= b.
+ *
+ * The intersection of two closed intervals is a set of real numbers that are either empty or represented as a closed interval.
+ * For example, the intersection of [1, 3] and [2, 4] is [2, 3].
+ *
+ * Example 1:
+ * Input: firstList = [[0,2],[5,10],[13,23],[24,25]], secondList = [[1,5],[8,12],[15,24],[25,26]]
+ * Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+ *
+ * Example 2:
+ * Input: firstList = [[1,3],[5,9]], secondList = []
+ * Output: []
+ *
+ * Example 3:
+ * Input: firstList = [], secondList = [[4,8],[10,12]]
+ * Output: []
+ *
+ * Example 4:
+ * Input: firstList = [[1,7]], secondList = [[3,10]]
+ * Output: [[3,7]]
+ *
+ * Constraints:
+ *
+ * 0 <= firstList.length, secondList.length <= 1000
+ * firstList.length + secondList.length >= 1
+ * 0 <= starti < endi <= 109
+ * endi < starti+1
+ * 0 <= startj < endj <= 109
+ * endj < startj+1
+ *
+ */
+
+public class Solution {
+    public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+        List<int[]> result = new ArrayList<>();
+        int i = 0, j = 0;
+
+        while(i < firstList.length && j < secondList.length) {
+            int[] tempA = firstList[i];
+            int[] tempB = secondList[j];
+
+            if(tempA[0] <= tempB[1] && tempB[0] <= tempA[1]) {
+                // Overlap
+                result.add(new int[]{Math.max(tempA[0], tempB[0]), Math.min(tempA[1], tempB[1])});
+            }
+
+            if(tempA[1] <= tempB[1]) // current range in A has end-value <= to end-value of the current range in B
+                i++;
+            else
+                j++;
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }    
+}
