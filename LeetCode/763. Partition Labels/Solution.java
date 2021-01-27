a28b223790b9ce517c49d914978a7b645353ab4b
Kalpak <kals9seals@gmail.com>
2021-01-26 22:03:10 -0500

    partitionLabels

--- LeetCode/763. Partition Labels/Solution.java
+++ LeetCode/763. Partition Labels/Solution.java
@@ -17,6 +17,49 @@
  */
 
 class Solution {
+    public List<Integer> partitionLabelsMergeIntervals(String S) {
+        List<Integer> result = new ArrayList<>();
+        Map<Character, int[]> map = new HashMap<>();
+        
+        // Create a map of character and their start and end indices.
+        for(int i = 0; i < S.length(); i++) {
+            char temp = S.charAt(i);
+            
+            if(!map.containsKey(temp)) {
+                int[] indices = new int[2];
+                indices[0] = i;
+                indices[1] = i;
+                map.put(temp, indices);
+            } else {
+                int[] indices = map.get(temp);
+                indices[1] = i;
+                map.put(temp, indices);
+            }
+        }
+        
+        for(int i = 0; i < S.length(); i++) {
+            char temp = S.charAt(i);
+            // Fetch the start and end index of the character from the map
+            int start = map.get(temp)[0];
+            int end = map.get(temp)[1];
+            
+            // Now check for characters within this interval. Update the interval if overlaps
+            while(i != end) {
+                // Go the next character
+                i++;
+                char ch = S.charAt(i);
+                int runningEndIndex = map.get(ch)[1];
+                
+                if(runningEndIndex > end) {
+                    // Overlapping interval. update the end
+                    end = runningEndIndex;
+                }
+            }
+            result.add(end - start + 1);    // Store the interval length
+        }
+        return result;
+    }
+
     public List<Integer> partitionLabels(String S) {
         List<Integer> result = new ArrayList<>();
         
